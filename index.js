b975fbc5d02988bdbfe040257f5f9926d69834da
sunyanbei <1779033346@qq.com>
2020-07-21 11:29:45 +0800

    fix bug

--- index.js
+++ index.js
@@ -2,26 +2,11 @@
 
 const fse = require('fs-extra');
 
-function Webhook(secret, path = './public', branch = 'master') {
-
-    function deleteall(path){
-        var files = [];
-        if(fse.existsSync(path)) {
-            files = fse.readdirSync(path);
-            files.forEach(function(file, index) {
-                var curPath = path + "/" + file;
-                if(fse.statSync(curPath).isDirectory()) { 
-                    deleteall(curPath);
-                } else { 
-                    fse.unlinkSync(curPath);
-                }
-            });
-            fse.rmdirSync(path);
-        }
-    };
+function Webhook(secret) {
 
     return async (ctx, next) => {
         let flag = false;
+        let brach_name = 'master';
         if(ctx.req.headers['x-github-event'] && ctx.req.headers['x-github-event'] === 'push'){//post from github hook 
             let signature = ctx.req.headers['x-hub-signature']; // extract signature from post
             if(signature){
@@ -31,14 +16,11 @@ function Webhook(secret, path = './public', branch = 'master') {
                     req.on('end', () => { resolve(data); });
                 })})(ctx.req);   
 
-                if(data.slice(19,19+branch.length) === branch){
+                if(data.slice(19,19+brach_name.length) === brach_name){
                     let crypto = require("crypto");
                     let hash='sha1=' + crypto.createHmac('sha1', secret).update(data).digest('hex'); //SHA1 decode
-
                     if(signature === hash){ 
                         // Validation successful
-                        // delete public
-                        deleteall(path);
                         flag = true;
                     }
                 }
