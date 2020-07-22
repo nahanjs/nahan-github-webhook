eed23de020400048bd80eeec27162ed0a70a715d
sunyanbei <1779033346@qq.com>
2020-07-22 15:00:53 +0800

    refactor code

--- index.js
+++ index.js
@@ -2,33 +2,45 @@
 
 const crypto = require("crypto");
 
+function getReqBody(req) {
+    return new Promise((resolve, reject) => { // GET body
+        let data = '';
+        req.on('data', (chunk) => { data += chunk; });
+        req.on('end', () => { resolve(data); });
+    });
+}
+
 function Webhook(secret) {
 
     return async (ctx, next) => {
+
         let flag = false;
         let brach_name = 'master';
-        if(ctx.req.headers['x-github-event'] && ctx.req.headers['x-github-event'] === 'push'){//post from github hook 
-            let signature = ctx.req.headers['x-hub-signature']; // extract signature from post
-            if(signature){
-                let data = await (req => {return new Promise((resolve, reject) => { // GET body
-                    let data = '';
-                    req.on('data', (chunk) => { data += chunk; });
-                    req.on('end', () => { resolve(data); });
-                })})(ctx.req);   
-                let data_json=JSON.parse(data);
-                if(typeof data_json == 'object' && data_json ){ //Check legal JSON
-                    let ref = data_json.ref;
-                    if(ref.split('/')[2] === brach_name){
-                        let hash='sha1=' + crypto.createHmac('sha1', secret).update(data).digest('hex'); //HmacSHA1 decode
-                        if(signature === hash){ 
-                            // Validation successful
-                            flag = true;
-                        }
-                    }
 
+        if (ctx.req.headers['x-github-event'] === 'push') { // post from github hook
+
+            const signature = ctx.req.headers['x-hub-signature']; // extract signature from post
+
+            if (signature) {
+
+                const data = ctx.github_webhook || await getReqBody(ctx.req);
+                ctx.github_webhook = data;
+
+                const json = JSON.parse(data);
+                const ref = json.ref;
+
+                if (ref.split('/')[2] === brach_name) {
+
+                    let hash = 'sha1=' + crypto.createHmac('sha1', secret).update(data).digest('hex'); //HmacSHA1 decode
+
+                    if (signature === hash) {
+                        // Validation successful
+                        flag = true;
+                    }
                 }
-            }                    
+            }
         }
+
         if (flag)
             await next(true);
         else
